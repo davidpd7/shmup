bb562fb24a49811fb9ae9899859dba353cb5a673
David <119945403+davidpd7@users.noreply.github.com>
2023-09-01 17:43:31 +0100

    changes on setup

--- shmup/setup.py
+++ shmup/setup.py
@@ -4,7 +4,7 @@ setup(
     name='shmup',
     version='0.01',
     author='David Perez',
-    author_email='david.perez@cubematch.com',
+    author_email='david.5697.9@gmail.com',
     packages=['shmup'],
     install_requires = ['pygame'],
     entry_points =  {
