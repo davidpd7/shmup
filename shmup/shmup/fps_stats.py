b2ef118c1c8c46a9189df39f4006b734e3562c59
davidpd7 <david.5697.9@gmail.com>
2023-09-04 17:07:02 +0100

    json config

--- shmup/shmup/fps_stats.py
+++ shmup/shmup/fps_stats.py
@@ -8,21 +8,22 @@ class FPSStats:
             self.__render_frames = 0
             self.__update_time = 0
             self.__set_fps_surface()
+            self.__max_update_time = cfg_item("timing","max_update_time")
 
     def update(self, delta_time):
         self.__logic_frames += 1 
         self.__update_time += delta_time
 
-        if self.__update_time >= cfg_item("max_update_time"):
+        if self.__update_time >= self.__max_update_time:
              self.__set_fps_surface()
              self.__logic_frames = 0
              self.__render_frames = 0
-             self.__update_time -= cfg_item("max_update_time")
+             self.__update_time -= cfg_item("timing","max_update_time")
              
     def render(self, surface):
         self.__render_frames += 1
-        surface.blit(self.__fps_surface, cfg_item("pos"))
+        surface.blit(self.__fps_surface, cfg_item("timing","fps_pos"))
 
     def __set_fps_surface(self):
            self.__fps_surface = self.__font.render(f"Update {self.__logic_frames} - Render {self.__render_frames}", 
-                                                   True, cfg_item("font_foreground_color"), cfg_item("background_color"))
+                                                   True, cfg_item("font","font_foreground_color"), cfg_item("game","background_color"))
